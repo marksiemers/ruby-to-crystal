0c10ebd7a9ba0caa9010c0daae118d602ddf4e2c
Mark Siemers <mark.siemers@gmail.com>
2017-12-03 21:09:42 -0800

    Add method-overloading

---
+++ method-overloading/crystal.cr
@@ -0,0 +1,26 @@
+module Type
+  extend self
+
+  def what_is_this(thing)
+    "This is definitely an Integer"
+  end
+
+  def what_is_this(thing)
+    "This is definitely an Array"
+  end
+
+  def what_is_this(thing)
+    "This is definitely a String"
+  end
+
+  def what_is_this(thing)
+    "This is definitely a Hash (of unknown types)"
+  end
+end
+
+puts Type.what_is_this(1)
+puts Type.what_is_this([] of Int32)
+# puts Type.what_is_this([] of String)
+puts Type.what_is_this("")
+puts Type.what_is_this({} of String => String)
+# puts Type.what_is_this({} of Symbol => String)
